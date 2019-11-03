9986fefd86172f93058cf37ada75cceb017f7f1c
Elco Garcia <garciaelco18@gmail.com>
2019-11-02 19:27:56 -0500

    added async function to  ask for cred

--- index.js
+++ index.js
@@ -13,3 +13,12 @@ if (files.directoryExists('.git')) {
 	console.log(chalk.red('Already a Git repository!'));
 	process.exit();
 }
+
+//test inquirer for input
+
+const run = async () => {
+	const credentials = await inquirer.askGithubCredentials();
+	console.log(credentials);
+};
+
+run();
